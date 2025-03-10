366814c908c11a0dff6588cf3f0bb6659ae603ec
Romashov111 <142166333+Romashov111@users.noreply.github.com>
2025-03-10 13:00:47 +0300

    server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import asyncio
+import websockets
+
+clients = set()
+
+async def handler(websocket, path):
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in clients:
+                if client != websocket:
+                    await client.send(message)
+    except:
+        pass
+    finally:
+        clients.remove(websocket)
+
+start_server = websockets.serve(handler, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
